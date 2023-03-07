130baad63dc70e025159466cc986d710f732f952
Surya <aplussurya@gmail.com>
2023-03-07 19:33:31 +0530

    ElectricEngine have Stopped

--- ElectricEngine.java
+++ ElectricEngine.java
@@ -2,4 +2,8 @@ public class ElectricEngine{
 	public class void main(String[] args) {
 		System.out.println("Electric Engine Started");
 	}
+	
+	public void stop() {
+		System.out.println("Electric Engine Stopped");
+	}
 }
