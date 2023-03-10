6f6e9cafcc624f1f209a57016a3a9ed9dc33241a
Surya <aplussurya@gmail.com>
2023-03-11 02:43:38 +0530

    Added power and fuel to diel engine

--- DieselEngine.java
+++ DieselEngine.java
@@ -5,4 +5,12 @@ public class DieselEngine{
 	public void stop() {		
 		System.out.println("Diesel Engine Stopped");
 	}
+	
+	public void generatePower() {		
+		System.out.println("Diesel Engine generating power");
+	}
+	
+	public void addingFuel() {		
+		System.out.println("Diesel Engine adding fuel");
+	}
 }
